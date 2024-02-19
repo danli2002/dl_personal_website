3067da8823ad0d87206b85aa17820fb811eeb923
Daniel Li <danielli@Daniels-MacBook-Pro-2.local>
2024-02-19 12:43:09 -0600

    Sidebar complete

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faTwitch, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+    <div className="nav-bar">
+      <Link className='logo' to='/'>
+        <img src={LogoS} alt='Logo'/>
+        <img className='sub-logo' src={LogoSubtitle} alt='LogoSubtitle'/>
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink exact="true" className="about-link"  activeclassname="active" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink exact="true" className="contact-link" activeclassname="active" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+    </div>
+)
+
+export default Sidebar
